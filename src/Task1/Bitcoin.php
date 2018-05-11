06cbbca1473ed50c6ef27f6c1ea6d0e97f471b77
ivanshamis <ivanshamis@gmail.com>
2018-05-11 14:40:25 +0300

    Bitcoin class created

---
+++ src/Task1/Bitcoin.php
@@ -0,0 +1,35 @@
+<?php declare(strict_types=1);
+
+namespace Cryptocurrency\Task1;
+
+class Bitcoin extends CryptoCurrency
+{
+    private $name;
+    private $logo;
+    private $price;
+
+    public function __construct(float $price)
+    {
+        $this->name = 'Bitcoin';
+        $this->logo = 'https://s2.coinmarketcap.com/static/img/coins/32x32/1.png';
+        $this->price = $price;
+    }    
+
+    public function getName(): string;
+    {
+        return this->name;
+    }
+
+    public function getDailyPrice(): float;
+    {
+        return this->price;
+    }
+
+    public function getLogoUrl(): string;
+    {
+        return this->logo;
+    }
+    
+}
+
+?>
